c516cedbaa794e99ff3f7f810b33fea058193e05
vincent deruaz <vincent.deruaz@he-arc.ch>
2014-10-30 16:18:58 +0100

    db modification

--- app/database/migrations/2014_10_30_141841_create_classes_table.php
+++ app/database/migrations/2014_10_30_141841_create_classes_table.php
@@ -19,7 +19,9 @@ class CreateClassesTable extends Migration {
 			$table->integer('id_school')->references('id')->on('schools');
 			$table->string('scollaryear',120);
 			$table->string('degree',120);
-			$table->
+			$table->string('domain',120);
+			$table->integer('previous')->references('id')->on('classes');
+			$table->string('visibility',120);
 		});
 	}
 
