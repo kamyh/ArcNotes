2d0d9863c7025a6a948749a1fe3a7e630ce36608
vincent deruaz <vincent.deruaz@he-arc.ch>
2014-11-13 14:53:41 +0100

    replace all sign string by create

---
+++ app/views/signclass.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.default')
+@section('body')
+<div class="">
+        <h2>new school</h2>
+        <?php
+        //TODO TEST
+        echo Session::get('isLogged');
+
+        $cantonList = DB::table('cantons')->lists('name','id');
+        $cityList = DB::table('cities')->distinct()->lists('name','id');
+
+        //TODO MODIFICATION
+        ?>
+
+        {{ Form::open(array('route' => array('school.store'), 'method' => 'post')) }}
+        @if($errors->any())
+            <div class="">
+                <a class="" data-dismiss="alert">&times;</a>
+                {{ implode('', $errors->all('<li class="error">:message</li>')) }}
+            </div>
+        @endif
+        <div class="">
+            {{Form::label('name','Name')}}
+            {{Form::text('name', null,array('class' => ''))}}
+        </div>
+
+        <div>
+            {{Form::label('canton','Canton')}}
+            {{ Form::select('canton', $cantonList, null, array('class' => '')) }}
+        </div>
+
+        <div>
+            {{Form::label('city','City')}}
+            {{ Form::select('city', $cityList, null, array('class' => '')) }}
+        </div>
+
+        {{Form::submit('Create', array('class' => ''))}}
+        {{ Form::close() }}
+</div>
+@stop
