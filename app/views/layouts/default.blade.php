bb896fb7ddc3e4753dcf8e6de50b9bfa01dd8945
nichuguen <nicolas.huguenin@he-arc.ch>
2014-11-18 11:25:54 +0100

    added login in profile-tile added link in ./

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -21,7 +21,31 @@
 		<div class="body row">
 			<div class="left-dock col scroll-y color-a">
 				<div class="profile">
-					PROFILE<br/>PROFILE<br/>PROFILE<br/>PROFILE<br/>
+				         @if(Session::get('isLogged') == 1)
+				            Welcome user: //missing
+                            {{ Form::open(array('url' => 'logout', 'method' => 'get')) }}
+                                {{Form::submit('Logout', array('class' => ''))}}
+                            {{ Form::close() }}
+
+                        @else
+
+                            {{ Form::open(array('url' => 'login', 'method' => 'post')) }}
+                                @if($errors->any())
+                                    <div class="">
+                                        <a class="" data-dismiss="alert">&times;</a>
+                                        {{ implode('', $errors->all('<li class="error">:message</li>')) }}
+                                    </div>
+                                @endif
+                                {{Form::label('email','Email')}}
+                                {{Form::text('email', null,array('class' => ''))}}        <br/>
+
+                                {{Form::label('password','Password')}}
+                                {{Form::password('password',array('class' => ''))}}        <br/>
+
+                                {{Form::submit('Login', array('class' => ''))}}
+                            {{ Form::close() }}
+                            <a href="/user/create">Sign up.</a>
+                        @endif
 				</div>
 				<div class="context-menus-dock scroll-y">
 					<?php generateLorem(10, "<div class=\"context-menu-tile hover-color-b\">context menu stuff</div>"); ?>
