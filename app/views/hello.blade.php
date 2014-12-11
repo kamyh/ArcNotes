687ff5a74100e5d92e5da33c12665156a9e028e6
vincent deruaz <vincent.deruaz@he-arc.ch>
2014-12-11 15:23:18 +0100

    page index refactor/remade

---
+++ app/views/hello.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.default')
+@section('title')
+Create a new course
+@endsection
+@section('body')
+	<div class="welcome">
+		<img src="img/logo.png" alt="ArcNotes">
+	</div>
+</body>
+</html>
+@stop
