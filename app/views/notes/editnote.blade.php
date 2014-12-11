1ccbbb98183a1834a25e4bdc62353465b67afcf6
Kilian Brandt <kilian.brandt-dit-grieurin@he-arc.ch>
2014-12-11 16:25:39 +0100

    english translated for notes management views

--- app/views/notes/editnote.blade.php
+++ app/views/notes/editnote.blade.php
@@ -8,15 +8,15 @@
     {{ Form::open(array('route' => array('/notes/update/{idnote}', 'idnote' => $idnote))); }}
     <table class="form">
         <tr>
-            <td>{{ Form::label('title', 'Titre', array('class' => 'form-label')) }}</td>
+            <td>{{ Form::label('title', 'Title', array('class' => 'form-label')) }}</td>
             <td>{{ Form::text('title', $title, array('size' => 100, 'maxlength' => 100, 'class' => 'text-input')); }}</td>
         </tr>
         <tr>
-            <td>{{ Form::label('content','Contenu', array('class' => 'form-label')); }}</td>
+            <td>{{ Form::label('content','Content', array('class' => 'form-label')); }}</td>
             <td>{{ Form::textarea('content', $content, array('cols' => 100, 'class' => 'textarea-input')); }}</td>
         </tr>
         <tr>
-           <td>&nbsp;</td><td>{{ Form::submit('Sauvegarder !', array('class' => 'button')); }} {{ Form::submit('Terminer',null, array('class' => 'button')); }}</td>
+           <td>&nbsp;</td><td>{{ Form::submit('Save', array('class' => 'button')); }} {{ Form::submit('Save and quit', array('class' => 'button')); }}</td>
         </tr>
     </table>
     {{Form::close();}}
--- app/views/notes/writenote.blade.php
+++ app/views/notes/writenote.blade.php
@@ -8,15 +8,15 @@
     {{ Form::open(array('route' => array('/notes/save/{idcourse}', 'idcourse' => $idcourse))); }}
     <table class="form">
         <tr>
-            <td>{{ Form::label('title', 'Titre', array('class' => 'form-label')) }}</td>
+            <td>{{ Form::label('title', 'Title', array('class' => 'form-label')) }}</td>
             <td>{{ Form::text('title', null, array('class' => 'text-input')); }}</td>
         </tr>
         <tr>
-            <td>{{ Form::label('content','Contenu', array('class' => 'form-label')); }}</td>
+            <td>{{ Form::label('content','Content', array('class' => 'form-label')); }}</td>
             <td>{{ Form::textarea('content', null, array('class' => 'textarea-input')); }}</td>
         </tr>
         <tr>
-            <td>&nbsp;</td><td>{{ Form::submit('Enregistrer !',array('class' => 'button')); }}</td>
+            <td>&nbsp;</td><td>{{ Form::submit('Create !',array('class' => 'button')); }}</td>
         </tr>
     </table>
     {{Form::close();}}
